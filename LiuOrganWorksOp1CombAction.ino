4a1fc92dcc00638dccc44360c065301cc8f1347d
Geoffrey Liu <g-liu@users.noreply.github.com>
2024-04-13 18:48:34 -0700

    POC with  1 button

--- LiuOrganWorksOp1CombAction.ino
+++ LiuOrganWorksOp1CombAction.ino
@@ -5,12 +5,29 @@
  * Combination action
  */
 
-void setup() {
-  // put your setup code here, to run once:
+const int BUTTON_PIN = A15; // the number of the pushbutton pin
 
+int lastState = HIGH; // the previous state from the input pin
+int currentState;    // the current reading from the input pin
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  Serial.println("Yeah we started");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  currentState = digitalRead(BUTTON_PIN);
 
+  if(lastState == LOW && currentState == HIGH) {
+    Serial.println("The state changed from LOW to HIGH");
+  } else if (lastState == HIGH && currentState == LOW) {
+    Serial.println("The state changed from HIGH to LOW");
+  }
+
+  // save the last state
+  lastState = currentState;
 }

8c2d689ef6ae0b1f02bcb4bf812a567a4212a81c
Geoffrey Liu <g-liu@users.noreply.github.com>
2024-04-13 19:55:52 -0700

    Midi over serial. I want Midi over USB >:(

--- LiuOrganWorksOp1CombAction.ino
+++ LiuOrganWorksOp1CombAction.ino
@@ -1,3 +1,5 @@
+#include <MIDI.h>
+
 /***
  * Liu Organ Works
  * Opus 1
@@ -5,13 +7,18 @@
  * Combination action
  */
 
+// Define the MIDI interface
+MIDI_CREATE_DEFAULT_INSTANCE();
+
 const int BUTTON_PIN = A15; // the number of the pushbutton pin
 
 int lastState = HIGH; // the previous state from the input pin
 int currentState;    // the current reading from the input pin
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(31250); // for MIDI
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+
   // put your setup code here, to run once:
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
@@ -24,10 +31,14 @@ void loop() {
 
   if(lastState == LOW && currentState == HIGH) {
     Serial.println("The state changed from LOW to HIGH");
+    MIDI.sendNoteOn(42, 127, 1);
   } else if (lastState == HIGH && currentState == LOW) {
     Serial.println("The state changed from HIGH to LOW");
+    MIDI.sendNoteOff(42, 127, 1);
   }
 
   // save the last state
   lastState = currentState;
+
+  MIDI.read();
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+LiuOrganWorksOp1CombAction
+=====
+
+The code for the combination action of my first digital pipe organ.
+
+Built with Arduino.
+
+Requires MIDI library:
+https://github.com/FortySevenEffects/arduino_midi_library

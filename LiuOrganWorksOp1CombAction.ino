74929d377ff935297520ab9abd6b9eb537ee2df3
Geoffrey Liu <g-liu@users.noreply.github.com>
2024-04-13 21:04:30 -0700

    Try to read all terminals... but WTF why am I getting no messages???

--- LiuOrganWorksOp1CombAction.ino
+++ LiuOrganWorksOp1CombAction.ino
@@ -1,4 +1,28 @@
+// #include <Control_Surface.h>
+
 #include <MIDI.h>
+
+const int NUM_BUTTONS = 55;
+const int ALL_BUTTON_PINS[NUM_BUTTONS] = {
+  A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, // Generals
+  22, 23, 24, 25, 26, 27, // Man III divisionals
+  7, 6, 5, 4, 3, 2, // Man II divisionals
+  13, 12, 11, 10, 9, 8, // Man I divisionals
+  37, 39, // Man III sequencers
+  36, 38, // Man II sequencers
+  15, 16, // Man I sequencers
+  35, 41, // Man III couplers
+  34, 40, 42, // Man II couplers
+  14, 17, A15, // Man I couplers
+  51, 53, // Man III assignables
+  50, 52, // Man II assignables
+  32, 33, // Man I assignables
+  18, // Setter
+  19 // General cancel
+};
+bool buttonStates[NUM_BUTTONS] = {0};
+
+
 
 /***
  * Liu Organ Works
@@ -10,35 +34,34 @@
 // Define the MIDI interface
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-const int BUTTON_PIN = A15; // the number of the pushbutton pin
-
-int lastState = HIGH; // the previous state from the input pin
-int currentState;    // the current reading from the input pin
-
 void setup() {
   Serial.begin(31250); // for MIDI
   MIDI.begin(MIDI_CHANNEL_OMNI);
 
-  // put your setup code here, to run once:
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    pinMode(ALL_BUTTON_PINS[i], INPUT_PULLUP);
+  }
 
   Serial.println("Yeah we started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  currentState = digitalRead(BUTTON_PIN);
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    bool currentState = digitalRead(ALL_BUTTON_PINS[i]);
+    bool lastState = buttonStates[i];
+    
+    if (currentState != lastState) {
+      buttonStates[i] = currentState;
 
-  if(lastState == LOW && currentState == HIGH) {
-    Serial.println("The state changed from LOW to HIGH");
-    MIDI.sendNoteOn(42, 127, 1);
-  } else if (lastState == HIGH && currentState == LOW) {
-    Serial.println("The state changed from HIGH to LOW");
-    MIDI.sendNoteOff(42, 127, 1);
+      if (currentState == HIGH) {
+        Serial.println("The state changed from LOW to HIGH for button " + ALL_BUTTON_PINS[i]);
+        MIDI.sendNoteOn(42, 127, 1);
+      } else {
+        Serial.println("The state changed from HIGH to LOW for button " + ALL_BUTTON_PINS[i]);
+        MIDI.sendNoteOff(42, 127, 1);
+      }
+    }
   }
-
-  // save the last state
-  lastState = currentState;
 
   MIDI.read();
 }
